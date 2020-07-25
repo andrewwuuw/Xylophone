be3da666eb337994b8c27a48027e536ecc4a9be2
chiaqingwu <secret8419@gmail.com>
2020-07-25 10:51:58 +0800

    fix: set the keyPressed to play sound

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,27 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        guard let senderTitle = sender.currentTitle else { return }
+        playSound(playFrom: senderTitle)
+    }
     
+    //MARK: - Private methods
+    private func playSound(playFrom label: String) {
+        let url = Bundle.main.url(forResource: label, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
 
 }
 
